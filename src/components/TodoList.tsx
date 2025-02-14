0c822c8a47c513f7ae32b0c8f395010e744a4dd2
Anna S. A <anna.atalay@minna.tech>
2025-02-14 21:04:39 +0100

    Fetch user-specific todos from Firestore based on authentication status

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,26 +3,36 @@ import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../redux/store'
 import { TodoType } from '../types/Types'
 import { useEffect } from 'react';
-import { getDocs } from 'firebase/firestore';
+import { getDocs, query, where } from 'firebase/firestore';
 import { setTodos } from '../redux/todoSlice';
 import { CSSTransition } from 'react-transition-group';
 import { todosCollection } from '../firebase';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function TodoList() {
   const dispatch = useDispatch();
   const { todos } = useSelector((state: RootState) => state.todo);
+  const { user, isAuthenticated } = useAuth0();
+
 
   useEffect(() => {
     const loadTodos = async () => {
-      const todoSnapshot = await getDocs(todosCollection);
-      const todoList = todoSnapshot.docs.map(doc => (doc.data()));
-      console.log('Remove me after test! Todos from DB:, ', todoList);
+      if (isAuthenticated && user) {
+        const userId = user.sub;
+        const userTodosQuery = query(todosCollection, where("userId", "==", userId));
+        const todoSnapshot = await getDocs(userTodosQuery);
+        const todoList = todoSnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        console.log('Remove me after test! User Todos from DB:', todoList);
 
-      dispatch(setTodos(todoList as TodoType[]));
+        dispatch(setTodos(todoList as TodoType[]));
+      }
     };
 
     loadTodos();
-  }, [dispatch]);
+  }, [dispatch, isAuthenticated, user]);
 
   return (
     <div className='todo-responsive'>
